b5efdca57594e8cd3d760a4bf6ee83f8164ad5fe
Greg DiCristofaro <gdicristofaro@fas.harvard.edu>
2018-02-17 22:01:39 -0500

    fix for easter calculation

--- src/ts/ParseHoliday.ts
+++ src/ts/ParseHoliday.ts
@@ -13,19 +13,20 @@ function arrayify(arr) {
 
 // taken from https://github.com/kodie/moment-holiday
 export default class ParseHoliday {
-    static calculateEaster(y) {
-        var c = Math.floor(y / 100);
-        var n = y - 19 * Math.floor(y / 19);
-        var k = Math.floor((c - 17) / 25);
-        var i = c - Math.floor(c / 4) - Math.floor((c - k) / 3) + 19 * n + 15;
-        i = i - 30 * Math.floor((i / 30));
-        i = i - Math.floor(i / 28) * (1 - Math.floor(i / 28) * Math.floor(29 / (i + 1)) * Math.floor((21 - n) / 11));
-        var j = y + Math.floor(y / 4) + i + 2 - c + Math.floor(c / 4);
-        j = j - 7 * Math.floor(j / 7);
-        var l = i - j;
-        var m = 3 + Math.floor((l + 40) / 44);
-        var d = l + 28 - 31 * Math.floor(m / 4);
-        return moment([y, (m - 1), d]);
+    static calculateEaster(Y) {
+        Y = parseInt(Y, 10);
+        var C = Math.floor(Y/100);
+        var N = Y - 19*Math.floor(Y/19);
+        var K = Math.floor((C - 17)/25);
+        var I = C - Math.floor(C/4) - Math.floor((C - K)/3) + 19*N + 15;
+        I = I - 30*Math.floor((I/30));
+        I = I - Math.floor(I/28)*(1 - Math.floor(I/28)*Math.floor(29/(I + 1))*Math.floor((21 - N)/11));
+        var J = Y + Math.floor(Y/4) + I + 2 - C + Math.floor(C/4);
+        J = J - 7*Math.floor(J/7);
+        var L = I - J;
+        var M = 3 + Math.floor((L + 40)/44);
+        var D = L + 28 - 31*Math.floor(M/4);
+        return moment([Y, (M - 1), D]);
     }
 
     static EasterParser(date) : any {
